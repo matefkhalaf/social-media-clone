26ba299ad87684b46df4f2d478e94ae9968a1ae2
Mohamed Khalaf <matefkhalaf@gmail.com>
2021-10-10 19:26:26 +0200

    remove screenshots

--- set_env.sh
+++ set_env.sh
@@ -1,2 +1,2 @@
-export DATABASECONNECTION="mongodb+srv://admin:admin@cluster0.mmbll.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+export DATABASECONNECTION=""
 export SESSION_SECRET=session_secret
